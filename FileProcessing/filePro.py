2347b1f4325aa2a03b2bc6337572aa60ffd8e9de
lvlaxjh <2876363140@qq.com>
2021-05-18 10:18:05 +0800

    Update filePro.py

--- FileProcessing/filePro.py
+++ FileProcessing/filePro.py
@@ -1,17 +1,18 @@
 '''
 Author: your name
 Date: 2021-05-18 09:51:21
-LastEditTime: 2021-05-18 10:11:23
+LastEditTime: 2021-05-18 10:17:52
 Description: file content
 '''
 import linecache
 import openpyxl
 CODE_FILE_PATH = '/Users/lvlaxjh/code/CBFL/AST/EclipseAST/App.java'
 EXCEL_FILE_PATH = '/Users/lvlaxjh/code/CBFL/FileProcessing/123.xlsx'
-excel = openpyxl.load_workbook(EXCEL_FILE_PATH)
-sheet = excel.worksheets[0]
-line = 2
-codeLineList = []
+excel = openpyxl.load_workbook(EXCEL_FILE_PATH)#excel文件
+sheet = excel.worksheets[0]#表
+line = 2#sheet 行
+codeLineList = []#存储代码行
+#读sheet-start
 while True:
     if sheet.cell(line, 2).value != None:
         codeLineList.append(sheet.cell(line, 2).value)
@@ -19,10 +20,13 @@ while True:
         break
     line+=1
 line = 2
+#读sheet-end
+#读代码，写sheet-start
 for i in codeLineList:
     the_line = linecache.getline(CODE_FILE_PATH, i)
+    # print(the_line)
     sheet.cell(line, 3).value = the_line
     line +=1
-# line = 5
-excel.save(EXCEL_FILE_PATH)
+#读代码，写sheet-end
+excel.save(EXCEL_FILE_PATH)#存储文件
 
