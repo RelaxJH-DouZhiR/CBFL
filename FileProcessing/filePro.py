f3fd322da350fa5722a1a6ea7bfc7a0dd079ae1d
RelaxJH-DouZhiR <44161928+RelaxJH-DouZhiR@users.noreply.github.com>
2021-05-18 16:17:50 +0800

    filepro

--- FileProcessing/filePro.py
+++ FileProcessing/filePro.py
@@ -1,13 +1,13 @@
 '''
 Author: your name
 Date: 2021-05-18 09:51:21
-LastEditTime: 2021-05-18 10:17:52
+LastEditTime: 2021-05-18 16:16:48
 Description: file content
 '''
 import linecache
 import openpyxl
 CODE_FILE_PATH = r'F:\崔老师组会文件\缺陷定位工作\源代码缺陷定位\lang-direct\lang-direct\lang-30\src\main\java\org\apache\commons\lang3\math\NumberUtils.java'
-EXCEL_FILE_PATH = 'F:\崔老师组会文件\缺陷定位工作\源代码缺陷定位\excels_lang\lang30.xlsx'
+EXCEL_FILE_PATH = r'C:\code\CBFL\FileProcessing\123.xlsx'
 excel = openpyxl.load_workbook(EXCEL_FILE_PATH)#excel文件
 sheet = excel.worksheets[0]#表
 line = 2#sheet 行
@@ -23,8 +23,9 @@ line = 2
 #读sheet-end
 #读代码，写sheet-start
 for i in codeLineList:
-    the_line = linecache.getline(CODE_FILE_PATH, i)
-    print(the_line.rstrip("\n"))
+    the_line = linecache.getline(
+        CODE_FILE_PATH+str(sheet.cell(line, 1).value).replace(".", "\\")+'.java', i)
+    # print(the_line.rstrip("\n"))
     sheet.cell(line, 3).value = the_line.rstrip("\n")
     line +=1
 #读代码，写sheet-end
