0a929a95b98971d5d735f25e8ed292806c040f8a
whr0724 <61044270+whr0724@users.noreply.github.com>
2021-05-18 16:07:34 +0800

    Update filePro.py

--- FileProcessing/filePro.py
+++ FileProcessing/filePro.py
@@ -6,8 +6,8 @@ Description: file content
 '''
 import linecache
 import openpyxl
-CODE_FILE_PATH = '/Users/lvlaxjh/code/CBFL/AST/EclipseAST/App.java'
-EXCEL_FILE_PATH = '/Users/lvlaxjh/code/CBFL/FileProcessing/123.xlsx'
+CODE_FILE_PATH = r'F:\崔老师组会文件\缺陷定位工作\源代码缺陷定位\lang-direct\lang-direct\lang-30\src\main\java\org\apache\commons\lang3\math\NumberUtils.java'
+EXCEL_FILE_PATH = 'F:\崔老师组会文件\缺陷定位工作\源代码缺陷定位\excels_lang\lang30.xlsx'
 excel = openpyxl.load_workbook(EXCEL_FILE_PATH)#excel文件
 sheet = excel.worksheets[0]#表
 line = 2#sheet 行
@@ -24,8 +24,8 @@ line = 2
 #读代码，写sheet-start
 for i in codeLineList:
     the_line = linecache.getline(CODE_FILE_PATH, i)
-    # print(the_line)
-    sheet.cell(line, 3).value = the_line
+    print(the_line.rstrip("\n"))
+    sheet.cell(line, 3).value = the_line.rstrip("\n")
     line +=1
 #读代码，写sheet-end
 excel.save(EXCEL_FILE_PATH)#存储文件
