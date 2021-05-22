76388f01eb3a62d515a6f899321133f9c9d88b4a
whr0724 <61044270+whr0724@users.noreply.github.com>
2021-05-22 10:44:16 +0800

    data
    data

--- FileProcessing/filePro.py
+++ FileProcessing/filePro.py
@@ -6,8 +6,8 @@ Description: file content
 '''
 import linecache
 import openpyxl
-CODE_FILE_PATH = r'F:\崔老师组会文件\缺陷定位工作\源代码缺陷定位\chart-direct\chart-direct\chart-24\source'
-EXCEL_FILE_PATH = r'F:\崔老师组会文件\缺陷定位工作\源代码缺陷定位\excels_chart\chart24.xlsx'
+CODE_FILE_PATH = r'F:\崔老师组会文件\缺陷定位工作\源代码缺陷定位\time-direct\time-direct\time-17\src\main\java'
+EXCEL_FILE_PATH = r'F:\崔老师组会文件\缺陷定位工作\源代码缺陷定位\excels_time\time17.xlsx'
 excel = openpyxl.load_workbook(EXCEL_FILE_PATH)#excel文件
 sheet = excel.worksheets[0]#表
 line = 2#sheet 行
