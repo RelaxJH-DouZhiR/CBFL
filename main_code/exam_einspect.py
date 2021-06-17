77abaf0e1a8682fb9b5487ff12a423c38378ecfc
lvlaxjh <2876363140@qq.com>
2021-06-17 16:06:03 +0800

    new data

--- main_code/exam_einspect.py
+++ main_code/exam_einspect.py
@@ -1,7 +1,7 @@
 '''
 Author: your name
 Date: 2021-06-15 16:04:17
-LastEditTime: 2021-06-16 09:41:52
+LastEditTime: 2021-06-17 16:02:48
 Description: file content
 '''
 import json
@@ -34,6 +34,31 @@ def get_old_EXAM(project, version):
 # 获取原文件EXAM-end
 
 
+# 获取新文件EXAM-start
+def get_new_EXAM(project, version):  # input:项目,版本;return:EXAM值
+    EXCEL_PATH = f'{FATHER_PATH}CBFL/data/{project}/{project}-{version}.xlsx'
+    excel = openpyxl.load_workbook(EXCEL_PATH)  # excel文件
+    sheet = excel.worksheets[0]  # 表
+    line = 2  # sheet 行
+    new_line = 2  # 新的行计算，去除所有预测为0的情况的
+    EXAM_line = -1  # EXAM的行
+    while True:
+        if sheet.cell(line, 1).value != None:
+            if sheet.cell(line, 6).value == 1 and EXAM_line == -1:
+                EXAM_line = new_line
+            if sheet.cell(line, 7).value == 1 or sheet.cell(line, 7).value == None:
+                new_line += 1
+            #
+            line += 1
+        else:
+            break
+    if EXAM_line == -1:
+        return 0
+    else:
+        return (EXAM_line-1)/(line-1)
+# 获取新文件EXAM-end
+
+
 # 获取原文件Einspect-start
 def get_old_Einspect(project, version, step):
     EXCEL_PATH = f'{FATHER_PATH}CBFL/data/{project}/{project}-{version}.xlsx'
@@ -55,7 +80,32 @@ def get_old_Einspect(project, version, step):
 # 获取原文件Einspect-end
 
 
+# 获取新文件Einspect-start
+def get_new_Einspect(project, version, step):
+    EXCEL_PATH = f'{FATHER_PATH}CBFL/data/{project}/{project}-{version}.xlsx'
+    excel = openpyxl.load_workbook(EXCEL_PATH)  # excel文件
+    sheet = excel.worksheets[0]  # 表
+    line = 2  # sheet 行
+    new_line = 2  # 新的行计算，去除所有预测为0的情况的
+    falut_sum = 0  # 错误总数
+    while True:
+        if sheet.cell(line, 1).value != None:
+            if sheet.cell(line, 6).value == 1 and (new_line-1) <= step:
+                falut_sum += 1
+            if sheet.cell(line, 7).value == 1 or sheet.cell(line, 7).value == None:
+                new_line += 1
+            #
+            line += 1
+        if (new_line+10) > step:
+            break
+        else:
+            break
+    return falut_sum
+# 获取新文件Einspect-end
+
 # 获取所有预测结果-start
+
+
 def get_predict(project, percentageList, k):
     predictList = []
     predictDict = {
@@ -130,7 +180,16 @@ def save_predict(project, versionList, percentageList, predictList, k):
 
 
 if __name__ == "__main__":
-    for project in ['chart', 'lang', 'time', 'math', 'closure', 'mockito']:
+    resultList = []
+    resultDict = {
+        'project': '',
+        'old_EXAM':'',
+        'new_EXAM':'',
+        'old_Einspect': '',
+        'new_Einspect':'',
+    }
+    # for project in ['chart', 'lang', 'time', 'math', 'closure', 'mockito']:
+    for project in ['chart']:
         Einspect_step = 10  # *
         # project = 'mockito'  # *
         settingJson = open(f'{FATHER_PATH}CBFL/main_code/setting.json', 'r')
@@ -148,15 +207,53 @@ if __name__ == "__main__":
             if i not in removeVersionList:
                 versionList.append(i)
         # 计算版本列表-end
-        # for i in versionList:
-        #     pass
-            # old_EXAM = get_old_EXAM(project, i)
-            # print(
-            #     f'\033[1;36m >{project}-{str(i)}< -EXAM- {old_EXAM}  \033[0m')
+        average_EXAM_old = 0
+        average_EXAM_new = 0
+        sum_Einspect_old = 0
+        sum_Einspect_new = 0
+        EXAM_num = 0
+        for i in versionList:
+            old_EXAM = get_old_EXAM(project, i)
+            print(
+                f'\033[1;36m >{project}-{str(i)}< -old EXAM- {old_EXAM}  \033[0m')
+            new_EXAM = get_new_EXAM(project, i)
+            print(
+                f'\033[1;36m >{project}-{str(i)}< -new EXAM- {new_EXAM}  \033[0m')
+            if old_EXAM != new_EXAM:
+                average_EXAM_old += old_EXAM
+                average_EXAM_new += new_EXAM
+                EXAM_num+=1
             # # ----------------------------------------------------------------
-            # for Einspect_step in [3,5,7,9]:
-            #     old_Einspect = get_old_Einspect(project, i, Einspect_step)
-            #     print(
-            #     f'\033[1;35m >{project}-{str(i)}< -Einspect@n-{Einspect_step}- {old_Einspect}  \033[0m')
-        predictList = get_predict(project, percentageList, k)
-        save_predict(project, versionList, percentageList, predictList, k)
+            old_Einspect = get_old_Einspect(project, i, 10)
+            sum_Einspect_old += old_Einspect
+            print(
+                f'\033[1;35m >{project}-{str(i)}< -old Einspect@n-{Einspect_step}- {old_Einspect}  \033[0m')
+            new_Einspect = get_new_Einspect(project, i, 10)
+            sum_Einspect_new += new_Einspect
+            print(
+                f'\033[1;35m >{project}-{str(i)}< -new Einspect@n-{Einspect_step}- {new_Einspect}  \033[0m')
+        # print(average_EXAM_old/len(versionList))
+        # print(average_EXAM_new/len(versionList))
+        resultDict['project'] = project
+        resultDict['old_EXAM'] = average_EXAM_old/EXAM_num
+        resultDict['new_EXAM'] = average_EXAM_new/EXAM_num
+        resultList.append(deepcopy(resultDict))
+        # print(sum_Einspect_old)
+        # print(sum_Einspect_new)
+        EXAM_num =0
+        average_EXAM_old = 0
+        average_EXAM_new = 0
+        sum_Einspect_old = 0
+        sum_Einspect_new = 0
+        #
+        EXCEL_PATH = f'{FATHER_PATH}CBFL/data/res.xlsx'
+        excel = openpyxl.load_workbook(EXCEL_PATH)  # excel文件
+        sheet = excel.worksheets[0]  # 表
+        line = 2  # sheet 行
+        print(resultList)
+        for i in resultList:
+            sheet.cell(line, 1).value = project
+            sheet.cell(line, 2).value = i['old_EXAM']
+            sheet.cell(line, 3).value = i['new_EXAM']
+            line+=1
+        excel.save(EXCEL_PATH)
